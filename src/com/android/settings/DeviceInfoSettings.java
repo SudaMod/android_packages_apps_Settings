cd33410f17403aefeb79f4d1dc17f2687a0d55b8
ghbhaha <ghbhaha@gmail.com>
2015-02-23 00:12:59 +0800

    Add Version Type
    Change-Id: I147f1f6a4f9015b5f08fe9a348599cdfc5803dba

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -78,6 +78,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_MOD_VERSION_TYPE = "mod_version_type";
     private static final String KEY_MOD_VERSION = "mod_version";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
     private static final String KEY_CM_UPDATES = "cm_updates";
@@ -109,6 +110,9 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setValueSummary(KEY_MOD_VERSION, "ro.sm.version");
         findPreference(KEY_MOD_VERSION).setEnabled(true);
         setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
+        setValueSummary(KEY_MOD_VERSION_TYPE, "ro.sm.releasetype");
+        findPreference(KEY_MOD_VERSION_TYPE).setEnabled(true);
+
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);
