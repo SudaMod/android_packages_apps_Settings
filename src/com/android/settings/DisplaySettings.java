592b79c523891645a51a651a4234a4fe50f377c5
ghbhaha <ghbhaha@gmail.com>
2015-03-09 16:22:22 +0800

    fix
    Change-Id: Ic22a26139000d0f1219eaf7230d4dbcbbd19ff0f

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
  * Copyright (C) 2014 The CyanogenMod Project
- * Copyright (C) 2014 The SudaMod Project
+ * Copyright (C) 2015 The SudaMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
