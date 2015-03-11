df93c224b519e52930670b09e9d32a6f184e4e5c
ghbhaha <ghbhaha@gmail.com>
2015-03-11 14:43:26 +0800

    Settings:add screenshot(2/2),thanks to Euphoria-OS
    Change-Id: Id2cd034c63aa617d6a983b3e90b32674dc357669

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -141,6 +141,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_tile_screenshot;
+                break;
             case QSConstants.TILE_NIGHTMODE:
                 resourceName = "ic_qs_nightmode_on";
                 stringId = R.string.qs_tile_nightmode;
