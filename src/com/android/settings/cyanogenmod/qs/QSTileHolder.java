a08b97bd06d1ecd0ecf4774331fb17f14fe1db55
ghbhaha <ghbhaha@gmail.com>
2015-04-04 15:13:27 +0800

    add qs_tile_nightmode&&qs_tile_screenshot(2/2)
    Change-Id: I715bdfd34aa0501cb97e100cb872e9fe6dceba8c

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -141,6 +141,14 @@ public class QSTileHolder {
                 resourceName = "ic_qs_screen_timeout_vector";
                 stringId = R.string.qs_tile_screen_timeout;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_tile_screenshot;
+                break;
+            case QSConstants.TILE_NIGHTMODE:
+                resourceName = "ic_qs_nightmode_on";
+                stringId = R.string.qs_tile_nightmode;
+                break;
             case QSConstants.TILE_LIVE_DISPLAY:
                 resourceName ="ic_livedisplay_day";
                 stringId = R.string.live_display_title;
