56d825633e1555e532b6fdd76f03d7e505c291c0
Alexander Martinz <eviscerationls@gmail.com>
2015-11-23 00:16:40 +0100

    Development: add lazy dexopt entry
      * for the lazy ones to enable lazy dexopting
    
    Change-Id: I15f9dad9d951c95e52ea7831f58f40f62202b714
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -189,6 +189,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
 
+    private static final String LAZY_DEXOPT = "dexopt_lazy";
+
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
 
     private static final String TERMINAL_APP_PACKAGE = "com.android.terminal";
@@ -203,6 +205,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
 
     private static final String PERSISTENT_DATA_BLOCK_PROP = "ro.frp.pst";
+
+    private static final String PROP_LAZY_DEXOPT = "persist.sys.lazy.dexopt";
 
     private static final int REQUEST_CODE_ENABLE_OEM_UNLOCK = 0;
 
@@ -285,6 +289,7 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private SwitchPreference mShowAllANRs;
     private SwitchPreference mKillAppLongpressBack;
+    private SwitchPreference mLazyDexopt;
 
     private ListPreference mRootAccess;
     private Object mSelectedRootValue;
@@ -467,6 +472,11 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mResetSwitchPrefs.add(mShowAllANRs);
 
         mKillAppLongpressBack = findAndInitSwitchPref(KILL_APP_LONGPRESS_BACK);
+
+        mLazyDexopt = (SwitchPreference) findPreference(LAZY_DEXOPT);
+        mLazyDexopt.setChecked(SystemProperties.getBoolean(PROP_LAZY_DEXOPT, false));
+        mLazyDexopt.setOnPreferenceChangeListener(this);
+        mAllPrefs.add(mLazyDexopt);
 
         Preference hdcpChecking = findPreference(HDCP_CHECKING_KEY);
         if (hdcpChecking != null) {
@@ -2139,6 +2149,10 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             return true;
         } else if (preference == mKeepScreenOn) {
             writeStayAwakeOptions(newValue);
+            return true;
+        } else if (preference == mLazyDexopt) {
+            final boolean enabled = (Boolean) newValue;
+            SystemProperties.set(PROP_LAZY_DEXOPT, enabled ? "true" : "false");
             return true;
         }
         return false;
