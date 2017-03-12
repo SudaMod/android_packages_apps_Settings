79f6676cbce7b718812fdc3db76304bb463d087e
aromer <pengloveyang1990@gmail.com>
2017-03-12 10:58:13 +0800

    Set external settings icon tint mode to SRC_ATOP
    Change-Id: I1ec454e8eedbe6859c765d1cdf342f3c4438d175

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -19,6 +19,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.Icon;
+import android.graphics.PorterDuff.Mode;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
@@ -126,7 +127,7 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
                         tile.intent.getComponent().getPackageName())) {
                     // If this drawable is coming from outside Settings, tint it to match the
                     // color.
-                    tile.icon.setTint(tintColor.data);
+                    tile.icon.setTint(tintColor.data).setTintMode(Mode.SRC_ATOP);
                 }
             }
         }
