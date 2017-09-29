50b76267d4d79ca21e796a2d1b67236556c050e9
Aromer-room <pengloveyang1990@gmail.com>
2017-09-29 21:09:09 +0800

    Dashboard tile layout config [1/2]
    Change-Id: I6e6631d99821f8e6fc2e301b94c7adab00d3155c

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -52,6 +52,7 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.view.RotationPolicy;
 import com.android.settings.accessibility.ToggleFontSizePreferenceFragment;
 import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settingslib.RestrictedLockUtils;
@@ -97,10 +98,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_WALLPAPER = "wallpaper";
     private static final String KEY_VR_DISPLAY_PREF = "vr_display_pref";
 
+    private static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
     private Preference mFontSizePref;
 
     private TimeoutListPreference mScreenTimeoutPreference;
     private ListPreference mNightModePreference;
+    private ListPreference mDashboardColumns;
     private Preference mScreenSaverPreference;
     private SwitchPreference mLiftToWakePreference;
     private SwitchPreference mDozePreference;
@@ -235,6 +239,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     displayPrefs.removePreference(rotatePreference);
                 }
             }
+
+            mDashboardColumns = (ListPreference) findPreference(DASHBOARD_COLUMNS);
+            mDashboardColumns.setValue(String.valueOf(Settings.System.getInt(
+                    getContentResolver(), Settings.System.DASHBOARD_COLUMNS, DashboardSummary.mNumColumns)));
+            mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+            mDashboardColumns.setOnPreferenceChangeListener(this);
 
             DropDownPreference vrDisplayPref =
                     (DropDownPreference) findPreference(KEY_VR_DISPLAY_PREF);
@@ -456,6 +466,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
           int alpha = (Integer) objValue;
           Settings.System.putInt(getContentResolver(),
                   Settings.System.TRANSPARENT_VOLUME_DIALOG, alpha * 1);
+        }
+        if (preference == mDashboardColumns) {
+            Settings.System.putInt(getContentResolver(), Settings.System.DASHBOARD_COLUMNS,
+                    Integer.valueOf((String) objValue));
+            mDashboardColumns.setValue(String.valueOf(objValue));
+            mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+            return true;
         }
         if (preference == mNightModePreference) {
             try {
